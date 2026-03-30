504367b07194b654da01468a587a97a228232226
barmichev <barmichev@gmail.com>
2026-03-30 14:46:16 +0300

    Обновлена команда запуска Asan и Tsan после ревью. Подготовка к тестированию на Linux

--- NOTES.md
+++ NOTES.md
@@ -174,7 +174,7 @@ rustup component add rust-src --toolchain nightly
 
 ```shell
 RUSTFLAGS="-Zsanitizer=address" \
-cargo +nightly test -Zbuild-std --target x86_64-unknown-linux-gnu
+cargo +nightly test -Zbuild-std=std --target x86_64-unknown-linux-gnu
 ```
 
 #### Успешный результат
@@ -189,8 +189,11 @@ cargo +nightly test -Zbuild-std --target x86_64-unknown-linux-gnu
 
 ```shell
 RUSTFLAGS="-Zsanitizer=thread" \
-cargo +nightly test -Zbuild-std --target x86_64-unknown-linux-gnu
+cargo +nightly test -Zbuild-std=std --target x86_64-unknown-linux-gnu
 ```
+
+_Примечание: `--target` должен соответствовать целевой системе. Рекомендуется
+тестирование проводить на Linux._
 
 #### Успешный результат
 
--- display_tests.py
+++ display_tests.py
@@ -24,6 +24,47 @@ LOG_DIR = Path('artifacts/logs')
 LOG_FILENAME = '{timestamp}_tests.log'
 
 
+class OS(StrEnum):
+    """Доступные ОС для идентификации."""
+    WINDOWS = 'windows'
+    LINUX = 'linux'
+    MACOS = 'darwin'
+    UNKNOW = 'unknow'
+
+    @classmethod
+    def detect_os(cls) -> OS:
+        """Определить тип системы и вернуть элемент перечисления."""
+
+        os_sys = platform.system().lower()
+        for key in vars(cls):
+            if key.islower() or key == 'UNKNOW': continue
+
+            os_attr: OS = getattr(OS, key)
+            if os_attr.value == os_sys:
+                return os_attr
+
+        return OS.UNKNOW
+
+    def get_rust_target(self) -> str | None:
+        """Определить target на основе ОС."""
+        match self:
+            case self.LINUX:
+                return 'x86_64-unknown-linux-gnu'
+            case self.WINDOWS:
+                return 'x86_64-pc-windows-msvc'
+            case self.MACOS:
+                if platform.machine().lower() == 'arm64':
+                    return 'aarch64-apple-darwin'
+                else:
+                    return 'x86_64-apple-darwin'
+            case _:
+                return None
+
+
+"""Идентифицированный тип системы."""
+OS_NAME: OS = OS.UNKNOW
+
+
 @dataclass(frozen=True)
 class Step:
     """Шаги тестирования."""
@@ -31,6 +72,7 @@ class Step:
     cmd: list[str]
     env_overrides: dict[str, str] | None = None
     linux_only: bool = False
+    ignore: bool = False
 
     def run(self, log_file: Path) -> int | None:
         """Выполнить тест комплексно (с логированием)."""
@@ -76,7 +118,7 @@ class Step:
     def _self_validate(self):
         """Провести самопроверку."""
 
-        if self.linux_only and platform.system() != 'Linux':
+        if self.linux_only and OS_NAME != OS.LINUX:
             raise RuntimeError('только для Linux')
         if not tool_exists(self.cmd[0]):
             raise RuntimeError(f'не найден инструмент: {self.cmd[0]}')
@@ -131,25 +173,15 @@ def make_log_file() -> Path:
 def tool_exists(tool: str | os.PathLike[str]) -> bool:
     """Проверить существование вызываемого инструмента."""
     tool_str = os.fspath(tool)
-    return shutil.which(tool_str) is not None
+    return shutil.which(tool_str) is not None  # noqa
 
 
 def detect_target() -> str:
     """Определить `target` для текущей ОС."""
 
-    target: str = ''
-    match platform.system().lower():
-        case 'linux':
-            target = 'x86_64-unknown-linux-gnu'
-        case 'windows':
-            target = 'x86_64-pc-windows-msvc'
-        case 'darwin':
-            if platform.machine().lower() == 'arm64':
-                target = 'aarch64-apple-darwin'
-            else:
-                target = 'x86_64-apple-darwin'
-        case _:
-            raise RuntimeError('Неподдерживаемая платформа')
+    target = OS_NAME.get_rust_target()
+    if target is None:
+        raise RuntimeError('Неподдерживаемая платформа')
 
     return target
 
@@ -160,26 +192,30 @@ def main() -> None:
 
     # Обеспечить правильную работу ASan и TSan.
     key_test_args = ['--target', detect_target(), '--tests']
-    cmd = ['cargo', '+nightly', 'test', ' -Zbuild-std', *key_test_args]
+    cmd = ['cargo', '+nightly', 'test', '-Zbuild-std=std', *key_test_args]
 
     steps: list[Step] = [
         Step(
             name='cargo test -- --nocapture',
             cmd=['cargo', 'test', '--', '--nocapture'],
+            ignore=True
         ),
         Step(
             name='cargo +nightly miri test',
             cmd=['cargo', '+nightly', 'miri', 'test'],
+            ignore=True,
         ),
         Step(
             name='valgrind --leak-check=full cargo test --tests',
             cmd=['valgrind', '--leak-check=full', 'cargo', 'test', '--tests'],
             linux_only=True,
+            ignore=True,
         ),
         Step(
             name='valgrid (...) target/debug/demo',
             cmd=['valgrind', '-s', '--leak-check=full',
-                 '--show-leak-kinds=all', 'target/debug/demo']
+                 '--show-leak-kinds=all', 'target/debug/demo'],
+            ignore=True
         ),
         Step(
             name='ASan (nightly)',
@@ -188,14 +224,16 @@ def main() -> None:
                 'RUSTFLAGS': '-Zsanitizer=address',
                 'CARGO_TARGET_DIR': 'target/asan'
             },
+            ignore=False
         ),
         Step(
             name='TSan (nightly)',
-            cmd=cmd,
+            cmd=cmd + ['--', '--no-capture'],
             env_overrides={
                 'RUSTFLAGS': '-Zsanitizer=thread',
                 'CARGO_TARGET_DIR': 'target/tsan'
             },
+            ignore=False
         ),
     ]
 
@@ -203,6 +241,10 @@ def main() -> None:
     has_fail = False
 
     for step in steps:
+        if step.ignore:
+            results.append((step.name, 'IGNORE'))
+            continue
+
         code = step.run(log_file)
         if code is None:
             results.append((step.name, 'SKIP'))
@@ -226,4 +268,5 @@ def main() -> None:
 
 
 if __name__ == '__main__':
+    OS_NAME = OS.detect_os()
     main()
